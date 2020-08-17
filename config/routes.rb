10a3bac09991e1c57f3b3064471478e652ab10cf
Sho77986 <68266590+Sho77986@users.noreply.github.com>
2020-08-18 08:08:45 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'menus#index'
+  resources  :menus  do
+    resources :menus_status, only: [:index, :show]
+  end
+  resources :questions,  only: [:index, :create]
 end
