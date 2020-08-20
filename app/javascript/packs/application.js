0f76336b376f9309b318c77c131dd7cc8989b5c6
Sho77986 <68266590+Sho77986@users.noreply.github.com>
2020-08-20 18:55:59 +0900

    JSの修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("../index")
+require("qr_codes/index")
 require('jquery')
 
 
