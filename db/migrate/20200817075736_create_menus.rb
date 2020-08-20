caf7685f114ca8bee1dc34e112d81985228c6a7c
Sho77986 <68266590+Sho77986@users.noreply.github.com>
2020-08-20 18:58:51 +0900

    メニューのマイグレーション修正

--- db/migrate/20200817075736_create_menus.rb
+++ db/migrate/20200817075736_create_menus.rb
@@ -2,13 +2,70 @@ class CreateMenus < ActiveRecord::Migration[6.0]
   def change
     create_table :menus do |t|
       t.integer      :user_id     ,null: false  ,foreign_key: true 
-      t.integer      :category_id ,null: false 
-      t.string       :image       ,null: false 
-      t.string       :name        ,null: false 
-      t.string       :price       ,null: false 
-      t.string       :calorie     ,null: false 
-      t.string       :allergies   ,null: false 
-      t.string       :comment  
+      t.string       :recommend_image_one
+      t.string       :recommend_price_one  
+      t.string       :recommend_calorie_one   
+      t.string       :recommend_allergies_one 
+      t.string       :recommend_charm_one
+      t.string       :recommend_image_two  
+      t.string       :recommend_price_two
+      t.string       :recommend_calorie_two   
+      t.string       :recommend_allergies_two  
+      t.string       :recommend_charm_two
+      t.string       :recommend_image_three
+      t.string       :recommend_price_three
+      t.string       :recommend_calorie_three 
+      t.string       :recommend_allergies_three
+      t.string       :recommend_charm_three
+ 
+      t.string       :main_image_one
+      t.string       :main_price_one
+      t.string       :main_calorie_one  
+      t.string       :main_allergies_one
+      t.string       :main_charm_one
+      t.string       :main_image_two 
+      t.string       :main_price_two  
+      t.string       :main_calorie_two 
+      t.string       :main_allergies_two 
+      t.string       :main_charm_two
+      t.string       :main_imege_three
+      t.string       :main_price_three
+      t.string       :main_calorie_three
+      t.string       :main_allergies_three 
+      t.string       :main_charm_three
+
+      t.string       :sub_image_one
+      t.string       :sub_price_one   
+      t.string       :sub_calorie_one
+      t.string       :sub_allergies_one
+      t.string       :sub_charm_one
+      t.string       :sub_image_two
+      t.string       :sub_price_two 
+      t.string       :sub_calorie_two 
+      t.string       :sub_allergies_two 
+      t.string       :sub_charm_two
+      t.string       :sub_image_three
+      t.string       :sub_price_three
+      t.string       :sub_calorie_three
+      t.string       :sub_allergies_three 
+      t.string       :sub_charm_three
+
+      t.string       :other_image_one
+      t.string       :other_price_one  
+      t.string       :other_calorie_one 
+      t.string       :other_allergies_one 
+      t.string       :other_charm_one
+      t.string       :other_image_two 
+      t.string       :other_price_two   
+      t.string       :other_calorie_two  
+      t.string       :other_allergies_two 
+      t.string       :other_charm_two
+      t.string       :other_image_three 
+      t.string       :other_price_three
+      t.string       :other_calorie_three 
+      t.string       :other_allergies_three
+      t.string       :other_charm_three
+
       t.timestamps
     end
   end
