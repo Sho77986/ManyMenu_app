a43e1b83a5f6253d5360497f4736bb5422a5bf37
Sho77986 <68266590+Sho77986@users.noreply.github.com>
2020-08-22 12:33:22 +0900

    uploaderの実装

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,5 +1,22 @@
 class Menu < ApplicationRecord
 
+  mount_uploader :recommend_image_one, ImageUploader
+  mount_uploader :recommend_image_two, ImageUploader
+  mount_uploader :recommend_image_three, ImageUploader
+  
+  mount_uploader :main_image_one, ImageUploader
+  mount_uploader :main_image_two, ImageUploader
+  mount_uploader :main_image_three, ImageUploader
+
+  mount_uploader :sub_image_one, ImageUploader
+  mount_uploader :sub_image_two, ImageUploader
+  mount_uploader :sub_image_three, ImageUploader
+
+  mount_uploader :other_image_one, ImageUploader
+  mount_uploader :other_image_two, ImageUploader
+  mount_uploader :other_image_three, ImageUploader
+
+
 
   def User.search(search)
     if search != ""
@@ -8,4 +25,5 @@ class Menu < ApplicationRecord
       User.all
     end
   end
+
 end
